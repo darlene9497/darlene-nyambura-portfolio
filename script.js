36ae420150412c73c87454f67ccb432a15c7f51e
darlene9497 <darlene.nandabi@student.moringaschool.com>
2025-04-03 12:27:21 +0300

    :art: added event listeners to handle smooth scrolling

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// scroll to the top of the page when the logo is clicked
+document.getElementById('logo').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+// select all a tags with the nav-link class
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        // scroll to the section with the id specified in the href
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
+
 // opens the CV in a new browser when the "Download CV" button is clicked
 function openCV() {
     window.open('/assets/files/Darlene Nyambura Nandabi CV 2025.pdf', '_blank');
